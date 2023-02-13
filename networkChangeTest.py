ef11312e04e43c73cc1ffffbc0a9316dd877c197
engincan <engincanhcylmz@hotmail.com>
2023-02-13 17:51:57 +0300

    Test

--- networkChangeTest.py
+++ networkChangeTest.py
@@ -1,7 +1,6 @@
 import socket
 import sys
 import subprocess
-import osascript
 
 
 def get_network_name():
@@ -20,8 +19,9 @@ def main():
     while True:
         new_network_name = get_network_name()
         if new_network_name != old_network_name:
-            subprocess.call(['osascript','-e',f'display nofication "ağ değişikliği aldılandi: {new_network_name}" with title "ağ Değişikliği"'])
+            subprocess.call(['osascript','-e',f'display nofication "Ağ Değişikliği Algılandı: {new_network_name}" with title "engcn"'])
         old_network_name = new_network_name
-
     if __name__ == '__main__':
         main()
+
+
